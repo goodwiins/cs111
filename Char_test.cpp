1bc8a440f515afd86a936ec3ad242fa3e5f336ad
abdelghafour el bikha <allocs16@gmail.com>
2022-02-16 02:36:58 +0000

    update adding the sec exmple.

---
+++ Char_test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+char exit = 'x';
+
+    cout << "the value of 'x' is: " << exit << endl; 
+    return 0;
+}
---
+++ Perimeter.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+// program that calucl the Perimeter of triangle
+using namespace std;
+int main() {
+short length = 4;
+short height = 3;
+short answer = 0;
+
+	answer = (length + height) * 2;
+	cout << "The Perimeter of Triangle is: " << answer << endl;
+	return 0;
+
+
+
+
+
+}
