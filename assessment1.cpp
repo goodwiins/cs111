0877e5cfa32d858d7258a1e54bc4252fea30055a
abdelghafour el bikha <allocs16@gmail.com>
2022-02-23 01:44:25 +0000

    update

--- assessment1.cpp
+++ assessment1.cpp
@@ -1,8 +1,12 @@
-/* the aear of the circle is A = PI*R^2
+/* 
+programmer: Abdel El Bikha.
+
+This is algorithm development 1. This program was written on a windows computer using Word.
+
+the aear of the circle is A = PI*R^2
 step one store the vaule of pi in variable PI = 3.14
 step two entre the vaule of R.
 step three diplay the answer.
-
 need to use decim so we can you float datetype for this case, also we need to consider that float takes 4 bite of memoery alocation. 
 */
 #define _USE_MATH_DEFINES
