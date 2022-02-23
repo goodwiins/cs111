ce001fbab8016ba57b3e3c49e425fb08bc85a5d1
abdelghafour el bikha <allocs16@gmail.com>
2022-02-23 01:42:14 +0000

    fix for the grading system.

--- assessment1.cpp
+++ assessment1.cpp
@@ -1,12 +1,15 @@
-// the aear of the circle is A = PI*R^2
-// step one store the vaule of pi in variable PI = 3.14
-// step two entre the vaule of R.
-// step three diplay the answer.
+/* the aear of the circle is A = PI*R^2
+step one store the vaule of pi in variable PI = 3.14
+step two entre the vaule of R.
+step three diplay the answer.
 
-// need to use decim so we can you float datetype for this case, also we need to consider that float takes 4 bite of memoery alocation. 
+need to use decim so we can you float datetype for this case, also we need to consider that float takes 4 bite of memoery alocation. 
+*/
+#define _USE_MATH_DEFINES
+#include <cmath>
 #include <iostream>
+
 using namespace std;
-const double PI = 22.0 / 7.0;
 
 int main(){
     //this complie time binding
@@ -17,7 +20,7 @@ double area = 0.0;
     cin >> diameter; 
     //run time binding
     radius = diameter / 2.0;
-    area = (radius*radius)*PI;
+    area = (radius*radius)*M_PI;
 
     cout << "the area of the circle with diameter of 10 is " << area << endl;
     
