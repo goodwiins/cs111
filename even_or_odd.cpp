314f0bb189d74562c7fe45e37718863caa2ba018
abdelghafour el bikha <allocs16@gmail.com>
2022-03-02 02:29:44 +0000

    updated

--- even_or_odd.cpp
+++ even_or_odd.cpp
@@ -3,18 +3,17 @@
 using namespace std;
 
 int main(){
+long TheInteger = 0 ;
 
-long long store = 0;
-long long result = 0;
 
-        cout << "can you please entre a value " << endl;
-        cin >> store;
-        if (store % 2 == 0){
+        cout << "Entre a The Integer " << endl;
+        cin >> TheInteger;
 
-        cout << "The value entered by the user is: " << store << ". The value is even." << endl;
-        }
-        else {
-                cout << "The value entered by the user is: " << store << "." <<endl;
-        }
+        cout << "The value entered by the user is: " << TheInteger << "." <<endl;
+        
+        if ((TheInteger % 2) == 0)
+        
+            cout << "The value entered by the user is: " << TheInteger << ". The value is even." << endl;
+        
         return 0;
 }
