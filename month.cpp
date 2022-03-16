bc014beb842c1460d7abeaef125f1daf7dc3e4bf
abdelghafour el bikha <allocs16@gmail.com>
2022-03-16 00:35:26 +0000

    fix style

--- month.cpp
+++ month.cpp
@@ -4,41 +4,27 @@ using namespace std;
 int main(){
 long year = 0;
 long month = 0;
-bool LeapYear = 0;
+bool leapYear = 0;
 long days = 0;
 
     cout << "Entre the year\n";
     cin >> year;
+    cout << "Entre the month\n";
+    cin >> month;
 
-    if (year <= 0) {
-        cout << "Entre the year again\n";
-        cin >> year;
-        cout << "Entre the month\n";
-        cin >> month;
-    } else {
-        cout << "Entre the month\n";
-        cin >> month;
-    }
-        
- 
-    if (month < 0 || month > 12) {
-        cout << "The month entred is wrong, Please renter the month\n";
-        cin >> month;
-    }
+    if ((year > 0) && (month >=1) && (month <=12)){
+        leapYear = (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)? 1:0);
 
-
-    LeapYear = (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)? 1:0);
-
-    if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+        if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
         days = 31;
-    else 
-        if (month == 2)
-            days = (LeapYear == 1) ? 29 : 28;
-    else
-        days = 30;
-
-
-    cout << "The number of days in " << month << "/" << year << " is " << days << "." << endl;
-    // month, year, day, is leap year.
+        else 
+            if (month == 2)
+                days = (leapYear == 1) ? 29 : 28;
+        else
+            days = 30;
+    
+        cout << "The number of days in " << month << "/" << year << " is " << days << "." << endl;
+    } else
+        cout << "the values entired are wrong" << endl;
     return 0;
  }
