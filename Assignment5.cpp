eb02ba36b614031f93a9cae292c40c1ba09b35b3
abdelghafour el bikha <allocs16@gmail.com>
2022-03-22 17:43:02 +0000

    update 5

--- Assignment5.cpp
+++ Assignment5.cpp
@@ -9,9 +9,13 @@ int main(){
 long base = 0;
 long lenght = 0;
 long area = 0;
-long preimeter = 0;
+long perimeter = 0;
 long radius = 0;
 long diameter = 0;
+long volume = 0;
+long a = 0;
+long h = 0;
+long r = 0;
 char choice;
     cout << "Select a geometric object: \n 1  Circle\n 2 Equilateral Triangle \n 3 Square\n 4  Cone\n X  No Choice" << endl;
     cin >> choice;
@@ -24,27 +28,43 @@ switch(choice) {
     diameter = 2*radius;
 
     // code block
-    cout << "1ok";
+    cout << "The area and diameter of a circle having a radius of "<< radius << "are" << area << "and " << diameter << ", respectively."<< endl;
     break;
+
   case '2':
+    
+    cout << "enter the base";
+    cin >> a;
+    h = sqrt(3/2)*a;
+    area = (a+h)/2;
+    perimeter = 3*a;
     // code block
-    cout << "2";
+    cout <<"The area and perimeter of an equilateral triangle having a base length of "<< a <<"are " << area << "and " << perimeter <<", respectively. "<< endl;
     break;
-    case '3':
+
+  case '3':
     // code block
-    cout << "3";
+    
+    cout << "enter the base";
+    cin >> a;
+    area = a^2;
+    perimeter = 4*a;
+    cout << "The area and perimeter of a square having a side length of" << a << "are" << area << "and" << perimeter <<", respectively.";
     break;
-    case '4':
+  case '4':
     // code block
-    cout << "4";
+    cout << "entre r";
+    cin >> r;
+    cout << "entre h";
+    cin >> h;
+    volume = (1 / 3)*M_PI*r^2*h;
+    area = M_PI*r*(r+h^2+r^2); 
+    cout << "The volume and surface area of a cone have the radius" << r << "and height" << h <<  "is" << volume <<  "and" << area<< ", respectively.";
     break;
-    case 'x':
+
+  case 'x':
     // code block
     cout << "x";
-    break;
-    case 'X':
-    // code block
-    cout << "X";
     break;
   default:
     cout << "error" << endl;
